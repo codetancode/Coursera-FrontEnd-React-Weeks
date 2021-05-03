bb3fa50cb69465c14e68547c9d5c4ac256ed6815
codetancode <tanay20gupta18@gmail.com>
2021-05-03 17:49:11 +0530

    Components part 2 taking data up to super class

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,18 +1,30 @@
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes.js';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div>
-      <Navbar color="warning">
-        <div className="container">
-          <NavbarBrand href="/">Pagal Panti</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu />
-    </div>
-  );
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      dishes:DISHES
+    };
+  }
+ 
+  render() {
+    return (
+      <div>
+        <Navbar color="warning">
+          <div className="container">
+            <NavbarBrand href="/">Pagal Panti</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={ this.state.dishes }/>
+      </div>
+    );
+  }
 }
 
 export default App;
