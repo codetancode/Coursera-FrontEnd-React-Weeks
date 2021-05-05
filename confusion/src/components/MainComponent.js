dbac957504fe58cdbca59e9bef0bc2693658a107
codetancode <tanay20gupta18@gmail.com>
2021-05-06 01:50:22 +0530

    Adding different components dividing data in .js and rendering it. SP part1

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -4,8 +4,12 @@ import Menu from './MenuComponent.js';
 import DishDetail  from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Contact  from './ContactComponent';
 import { DISHES } from '../shared/dishes.js';
 import { Switch, Route, Redirect} from 'react-router-dom';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
 
 class Main extends Component{
 
@@ -13,7 +17,9 @@ class Main extends Component{
     super(props);
     this.state = {
       dishes:DISHES,
-      selectedDish:null
+      comments:COMMENTS,
+      promotions:PROMOTIONS,
+      leaders:LEADERS
     };
   }
   onDishSelect(dishId){
@@ -24,7 +30,9 @@ class Main extends Component{
   render() {
     const HomePage = () => {
       return(
-        <Home />
+        <Home dish={this.state.dishes.filter( (dish) => dish.featured )[0] }
+        promotion={this.state.promotions.filter( (promo) => promo.featured )[0] }
+        leader={this.state.leaders.filter( (lead) => lead.featured )[0] } />
       );
 
     }
@@ -34,8 +42,9 @@ class Main extends Component{
           <Switch>
             <Route path="/home" component={ HomePage } />
             <Route exact path="/menu" component={ () =>  <Menu dishes={this.state.dishes} /> }  />
+            <Route exact path="/contactus" component={Contact}/>
+
             <Redirect to="/home" />
-            
           </Switch>
         <Footer />
       </div>
